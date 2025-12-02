72f06b754c2dedef6ad35444b86d1c28b464f2e1
z <2647461091@qq.com>
2025-12-02 11:54:51 +0800

    init project

---
+++ src/main/java/xyz/rabbitdog/raffleplatform/RafflePlatformApplication.java
@@ -0,0 +1,13 @@
+package xyz.rabbitdog.raffleplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafflePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RafflePlatformApplication.class, args);
+    }
+
+}
